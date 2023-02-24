537a1a39bfc725df78d0733220f57a6684e5087d
Katherine <80862932+katherinek123@users.noreply.github.com>
2023-02-24 14:59:56 -0800

    added type userBalance and Query in userBalance.sdl.js

--- api/src/graphql/userBalance.sdl.js
+++ api/src/graphql/userBalance.sdl.js
@@ -1,1 +1,11 @@
-export const schema = gql``
+export const schema = gql`
+  type userBalance {
+    id: Int!
+    name: String!
+    amount: Int!
+  }
+
+  type Query {
+    userBalance(id: Int!): userBalance @requireAuth
+  }
+`
