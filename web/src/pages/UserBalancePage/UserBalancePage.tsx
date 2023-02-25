c81eab80b1e3b5dc0afda7daa7b2eef8f45b69ec
Katherine <80862932+katherinek123@users.noreply.github.com>
2023-02-24 19:17:16 -0800

    created simple Form

--- web/src/pages/UserBalancePage/UserBalancePage.tsx
+++ web/src/pages/UserBalancePage/UserBalancePage.tsx
@@ -1,14 +1,19 @@
+import { Form, Submit, FieldError, Label, TextField, TextAreaField,} from '@redwoodjs/forms'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags, useMutation } from '@redwoodjs/web'
-
 const CREATE_USER_BALANCE = gql`
   mutation CreateUserBalanceMutation($input: CreateUserBalance!) {
-    createUserBalance()
+    createUserBalance(input: $input) {
+      name
+    }
   }
 `
 
 const UserBalancePage = () => {
   const [create] = useMutation(CREATE_USER_BALANCE)
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="UserBalance" description="UserBalance page" />
@@ -16,8 +21,17 @@ const UserBalancePage = () => {
       <h1>UserBalancePage</h1>
       <p>
         THIS IS THE USER BALANCE
-        {() => create()}
       </p>
+      <Form onSubmit={onSubmit}>
+        <Label name="token" errorClassName="error">
+          Token Name
+        </Label>
+        <TextField name='token' validation={{
+          required: true
+        }} />
+        <FieldError name='token' className='error'></FieldError>
+        <Submit>Find My Balance</Submit>
+      </Form>
       <p>
         My default route is named <code>userBalance</code>, link to me with `
         <Link to={routes.userBalance()}>UserBalance</Link>`
