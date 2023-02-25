aaa7e26b18b16c29fa73423fea77947d9952603b
Katherine <80862932+katherinek123@users.noreply.github.com>
2023-02-24 17:50:34 -0800

    fixed type errors in services/userBalances.js

--- api/src/services/userBalances/userBalances.js
+++ api/src/services/userBalances/userBalances.js
@@ -1,5 +1,5 @@
 import { Contract, getDefaultProvider, providers, utils } from 'ethers'
-import type { QueryResolvers, MutationResolvers } from 'types/graphql'
+//import type { MutationResolvers } from 'types/graphql'
 
 import { db } from 'src/lib/db'
 
@@ -8,9 +8,9 @@ function getProvider(chain) {
     chain === 'ETH'
       ? getDefaultProvider()
       : new providers.JsonRpcProvider(
-        'https://www.ethercluster.com/etc',
-        'classic'
-      )
+          'https://www.ethercluster.com/etc',
+          'classic'
+        )
   return provider
 }
 
@@ -34,9 +34,16 @@ const contractAbiFragment = [
     payable: false,
   },
 ]
-
+//read only connection w eth blockchain
 const provider = getProvider('ETH')
+const token = 'DOGE'
+const account = '0x826B095b14EEE1dd05C3fd2f656020f8b0420494'
+//can be read from when connected with provider
 const contract = new Contract(token, contractAbiFragment, provider)
+const symbol = await contract.symbol()
+const balance = await contract.balanceOf(account)
+
+//all incoming units of USD are in cents
 
 // export const userBalances: QueryResolvers['userBalances'] = () => {
 //   return db.userBalance.findMany()
@@ -48,13 +55,14 @@ const contract = new Contract(token, contractAbiFragment, provider)
 //   })
 // }
 
-// export const createUserBalance: MutationResolvers['createUserBalance'] = ({
-//   input,
-// }) => {
-//   return db.userBalance.create({
-//     data: input,
-//   })
-// }
+export const createUserBalance = (symbol, balance) => {
+  return db.userBalance.create({
+    data: {
+      name: symbol,
+      amount: balance,
+    },
+  })
+}
 
 // export const updateUserBalance: MutationResolvers['updateUserBalance'] = ({
 //   id,
